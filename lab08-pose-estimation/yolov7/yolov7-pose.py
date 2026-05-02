1e1626e173a118481e270cda7648f3912bc31d91
Sajedah <saalqudaihi@gmail.com>
2026-05-03 00:17:19 +0300

    Completed pose estimation lab

--- lab08-pose-estimation/yolov7/yolov7-pose.py
+++ lab08-pose-estimation/yolov7/yolov7-pose.py
@@ -71,8 +71,7 @@ model.to(device)
 
 # Provide the list of paths to your chosen videos her
 videos = [
-        'skydiving',
-        'far-away']
+        'walking-persons']
 
 file_name = videos[0] + '.mp4'
 vid_path = '../media/' + file_name
@@ -87,8 +86,7 @@ h, w, _ = frame.shape
 #                       cv2.VideoWriter_fourcc(*'mp4v'), 
 #                       fps, (w, h))
 
-out = cv2.VideoWriter(f"{save_name}_yolo7.avi",cv2.VideoWriter_fourcc('M','J','P','G'), 10, w,h)
-
+out = cv2.VideoWriter(f"{file_name}_yolo7.avi", cv2.VideoWriter_fourcc('M','J','P','G'), 10, (w,h))
 #-------------------------------------------------------------------------------#
 
 
